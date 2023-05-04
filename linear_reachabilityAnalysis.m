277d063f6055ed021d7656f564e6e3e7d256e0c4
Arsalan Havaie <104509142+Arsalanhavaie@users.noreply.github.com>
2023-05-04 14:15:04 +0200

    Update tortoise_test.txt

---
+++ linear_reachabilityAnalysis.m
@@ -0,0 +1,145 @@
+
+%------------- BEGIN CODE --------------
+
+clear all
+
+
+% System Dynamics ---------------------------------------------------------
+
+[f,h] = nonLinear_Gsp();
+x_eq = [1 15 70];
+[A,B,C,D,u_eq] = linear_Gsp(f,h,x_eq);
+
+Sys = linearSys('linearizedSys',A,B);
+
+
+% Parameters --------------------------------------------------------------
+
+params.tFinal = 0.3;
+
+%  0 < x1 < 2
+%  0 < x2 < 50
+%  0 < x3 < 100
+params.R0 = zonotope([1-x_eq(1); 25-x_eq(2); 50-x_eq(3)], zeros(3));
+%params.R0 = zonotope(interval([0; 10; -20], [0; 10; -20]));
+
+%  0 < u1 < 400
+%  0 < u2 < 4
+%  0 < u3 < 400
+%params.U = zonotope(interval([0; 0; 0], [400; 4; 400]));
+params.U = zonotope([200-u_eq(1); 2-u_eq(2); 200-u_eq(3)],...
+                    [200 0 0; 0 2 0; 0 0 200]);
+
+
+% Reachability Settings ---------------------------------------------------
+
+options.timeStep = 0.01; 
+options.taylorTerms = 4;
+options.zonotopeOrder = 175; 
+% options.linAlg = 'adaptive'; % use adaptive parameter tuning 
+% options.error = 0.1;
+
+
+% Reachability Analysis ---------------------------------------------------
+
+safeSet = specification(zonotope(interval([0-x_eq(1); 0-x_eq(2); 0-x_eq(3)],...
+                                          [2-x_eq(1); 50-x_eq(2); 100-x_eq(3)])),...
+                                          'safeSet');
+tic;
+%Rin = reachInner(Sys,params,options);
+Rout = reach(Sys,params,options,safeSet);
+tComp = toc;
+%stepssS = length(R.timeInterval.set);
+disp(['computation time of reachable set: ',num2str(tComp)]);
+
+% Fault model -------------------------------------------------------------
+
+u1 = 0;
+fault1.tFinal = Rout.timePoint.time{length(Rout.timePoint.time)};
+fault1.R0 = zonotope([1-x_eq(1); 25-x_eq(2); 50-x_eq(3)], zeros(3));
+fault1.U = zonotope([u1-u_eq(1); 2-u_eq(2); 200-u_eq(3)],...
+                    [0.0001 0 0; 0 2 0; 0 0 200]);
+ 
+u2 = 0;
+fault2.tFinal = Rout.timePoint.time{length(Rout.timePoint.time)};
+fault2.R0 = zonotope([1-x_eq(1); 25-x_eq(2); 50-x_eq(3)], zeros(3));
+fault2.U = zonotope([200-u_eq(1); u2-u_eq(2); 200-u_eq(3)],...
+                    [200 0 0; 0 0.0001 0; 0 0 200]);
+
+u3 = 0;
+fault3.tFinal = Rout.timePoint.time{length(Rout.timePoint.time)};
+fault3.R0 = zonotope([1-x_eq(1); 25-x_eq(2); 50-x_eq(3)], zeros(3));
+fault3.U = zonotope([200-u_eq(1); 2-u_eq(2); u3-u_eq(3)],...
+                    [200 0 0; 0 2 0; 0 0 0.0001]);
+
+
+R_u1 = reach(Sys,fault1,options,safeSet);
+R_u2 = reach(Sys,fault2,options,safeSet);
+R_u3 = reach(Sys,fault3,options,safeSet);
+
+
+% Redundancy Analysis------------------------------------------------------
+
+% R_red = reachSet;
+% R_red.timePoint.time = R.timePoint.time;
+% R_red.timePoint.error = R.timePoint.error;
+% R_red.timeInterval.time = R.timeInterval.time;
+% R_red.timeInterval.error = R.timeInterval.error;
+
+% for i = 1:stepssS
+% 
+%     R_red.timePoint.set{i,1} = R.timePoint.set{i} & R_u1.timePoint.set{i};
+%     R_red.timeInterval.set{i,1} = R.timeInterval.set{i} & R_u1.timeInterval.set{i};
+% 
+% end
+
+
+% Simulation --------------------------------------------------------------
+
+simOpt.points = 50;
+simOpt.type = 'gaussian';
+
+% simRes = simulateRandom(Sys, params, simOpt);
+% simRes = simulateRandom(Sys, fault1, simOpt);
+% simRes = simulateRandom(Sys, fault2, simOpt);
+simRes = simulateRandom(Sys, fault3, simOpt);
+
+
+% Visualization -----------------------------------------------------------
+
+% plot different projections
+dims = {[1 2],[1,3]};
+%dims = {[1 2 3]};
+
+for k = 1:length(dims)
+
+    figure; hold on; box on
+    useCORAcolors("CORA:contDynamics")
+    projDims = dims{k};
+
+    % plot reachable sets
+    %plot(R,projDims, 'DisplayName', 'Reachable set');
+    useCORAcolors("CORA:contDynamics", 3)
+     
+    plot(Rout, projDims, 'DisplayName', sprintf("Reachable set over-approximation")); 
+    %plot(Rin, projDims, 'DisplayName', sprintf("Reachable set inner-approximation")); 
+    %plot(safeSet,projDims,'DisplayName', sprintf('Safe Set'));
+
+    % plot(R_u1, projDims, 'DisplayName', sprintf("u1 = 0"));
+    % plot(R_u2, projDims, 'DisplayName', sprintf("u2 = 0")); 
+    plot(R_u3, projDims, 'DisplayName', sprintf("u3 = 0")); 
+
+    % plot initial set
+    % plot(R.R0,projDims,'DisplayName','Initial set');
+
+    % plot simulation results
+    plot(simRes,projDims, 'DisplayName', 'Simulations');
+
+    % label plot
+    xlabel(['x_{',num2str(projDims(1)),'}']);
+    ylabel(['x_{',num2str(projDims(2)),'}']);
+    legend('Location', 'northwest')
+end
+
+
+%------------- END OF CODE --------------
