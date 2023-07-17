7ba4c19a18c5c3b0d005171da3be2f86cd3fc2ee
Shengping Yu <shengpingyu@qq.com>
2023-07-17 12:37:16 +0200

    new eq

--- system_def.m
+++ system_def.m
@@ -2,7 +2,7 @@ function sys = system_def()
 %% this function is for all system parameter
     sys = struct;
     [sys.f, sys.h] = nonLinear_Gsp();
-    sys.x_eq = [1 15 70];
+    sys.x_eq = [0.9966 25.2981 51.5618];
     [sys.A,sys.B,sys.C,sys.D,sys.u_eq] = linear_Gsp(sys.f,sys.h,sys.x_eq);
     sys.constraint_x = [2;50;100];
     sys.constraint_u = [400;4;400];
