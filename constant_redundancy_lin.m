51af36cb518400446c819d85dc0b633b6d49aefa
Shengping Yu <shengpingyu@qq.com>
2023-09-18 22:00:58 +0200

    before merging, has test file and visulization file

--- constant_redundancy_lin.m
+++ constant_redundancy_lin.m
@@ -11,7 +11,7 @@ function Result = constant_redundancy_lin(sys)
     constraint_u = sys.constraint_u;
     x0 = [constraint_x(1)/2-x_eq(1); constraint_x(2)/2-x_eq(2); constraint_x(3)/2-x_eq(3)];
     u0 = [constraint_u(1)/2-u_eq(1); constraint_u(2)/2-u_eq(2); constraint_u(3)/2-u_eq(3)];
-    result = struct;
+    Result = struct;
 
 
 
@@ -19,71 +19,46 @@ function Result = constant_redundancy_lin(sys)
     analyinput = struct;
     analyinput.R0 = zonotope(x0, zeros(3));
     redundancystates = redundancy_init(sys,"linear");
-    analyinput.timestep = 6;
-
+    analyinput.timestep = sys.simtime;
     %% Analyze
-
-    % analyze all single redundancies
-    num_single = size(redundancystates.single,1);
-    input_single = redundancystates.single;
-    t_single = tic;
-   for i = 1:num_single
-        U_generator = diag([input_single(i,4),input_single(i,5),input_single(i,6)]);
-        U_center = [input_single(i,1),input_single(i,2),input_single(i,3)];
-        U = zonotope(U_center',U_generator);
-        [res_single{i}, R_single{i}] = reach_analysis_lin_dis(sys,analyinput,U);
+    n = max(size(u_eq));% n is dimension of inputs
+    for i = 1:(2*n+1)
+        u0 = [constraint_u(1)/2-u_eq(1); constraint_u(2)/2-u_eq(2); constraint_u(3)/2-u_eq(3)];
+        U_generator = constraint_u/2;
+        if i == 2*n+1
+            U_generator = constraint_u/2;
+            U = zonotope(u0,U_generator);
+            [Sim{i}, R{i}] = reach_analysis_lin_dis(sys,analyinput,U);
+        elseif mod(i,2) == 1 %lower bound
+            U_generator((i+1)/2) = 0;
+            u0((i+1)/2) = 0-u_eq((i+1)/2);
+            U = zonotope(u0,U_generator);
+            [Sim{i}, R{i}] = reach_analysis_lin_dis(sys,analyinput,U);
+        elseif mod(i,2) == 0 %upper bound
+            U_generator(i/2) = 0;
+            u0(i/2) = constraint_u(i/2)-u_eq(i/2);
+            U = zonotope(u0,U_generator);
+            [Sim{i}, R{i}] = reach_analysis_lin_dis(sys,analyinput,U);
+        end
     end
-    toc(t_single);
-    % with time = 3, 60 loops, total analysis time for single redundancy is 45.27
-    % seconds (around 40 seconds)with parallel computing, without visulization
+    Result.Sim = Sim;
+    Result.R = R;    
 
-%     % analyze all double redundancies
-%     num_double = size(redundancystates.double,1);
-%     input_double = redundancystates.double;
-%     t_double = tic;
-%     parfor i = 1:num_double
-%         U_generator = diag([input_double(i,1),input_double(i,2),input_double(i,3)])
-%         [res_double{i}, R_double{i}] = reach_analysis_lin_dis(sys,analyinput,U_generator);
+
+%%%% analyze all single constant failure mode%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%     num_single = size(redundancystates.single,1);
+%     input_single = redundancystates.single;
+%     t_single = tic;
+%     for i = 1:num_single
+%         U_generator = diag([input_single(i,4),input_single(i,5),input_single(i,6)]);
+%         U_center = [input_single(i,1),input_single(i,2),input_single(i,3)];
+%         U = zonotope(U_center',U_generator);
+%         [res_single{i}, R_single{i}] = reach_analysis_lin_dis(sys,analyinput,U);
 %     end
-%     toc(t_double);
-%     %1200 loops, takes around 800 seconds
-
-    % analyze triple redundancies
-%     num_triple = size(redundancystates.triple,1);
-%     input_triple = redundancystates.triple;
-%     t_triple = tic;
-%     parfor i = 1:num_triple
-%         U_generator = diag([input_triple(i,1),input_triple(i,2),input_triple(i,3)])
-%         [res_triple{i}, R_triple{i}] = reach_analysis_lin_dis(sys,analyinput,U_generator);
-%     end
-%     toc(t_triple);
-    %
-    %% To be continued not tested for double and triple redundancy
-%     simres = [res_single res_double res_triple];
-%     R = [R_single R_double R_triple];
-
-    
-    R = R_single;
-    timesteps = size(R{1}.timePoint.set,1);
-%     bundle ={};
-    for i = 1:timesteps
-        A = R{1}.timePoint.set(i);
-        B = R{2}.timePoint.set(i);
-        inter_set(i,1) = A{1,1} & B{1,1};
-%         bundle{1} = A{1,1};
-%         bundle{2} = B{1,1};
-%         res_R(i) = zonoBundle(bundle);
-    end
-    set_limit_u1l = boundary_of_zonotope(R{1}.timePoint.set);
-    set_limit_u1r = boundary_of_zonotope(R{2}.timePoint.set);
-    set_limit_inter = boundary_of_zonotope(inter_set);
-    plot_interval_overtime(set_limit_inter.x1);
-    
-
-%     timerange = size(res_R,1);
-%     parfor i = 1:timerange
-%         Result{1,i} = zonoBundle(res_R(i,:));
-%     end
+%     toc(t_single);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 end
--- reach_analysis_lin_dis.m
+++ reach_analysis_lin_dis.m
@@ -8,7 +8,7 @@ function [simRes, R]= reach_analysis_lin_dis(sys,input,U)
     x_eq = sys.x_eq;
     % Reachability Settings ---------------------------------------------------
     
-    options.timeStep = 0.3; 
+    options.timeStep = sys.timestep; 
     options.taylorTerms = 4;
     options.zonotopeOrder = 20; 
     % System Dynamics ---------------------------------------------------------
@@ -19,10 +19,16 @@ function [simRes, R]= reach_analysis_lin_dis(sys,input,U)
 %                                           [2-x_eq(1); 50-x_eq(2); 100-x_eq(3)])),...
 %                                           'safeSet');
 
-safeSet = zonotope(interval([0-x_eq(1); 0-x_eq(2); 0-x_eq(3)],...
+    safeSet = zonotope(interval([0-x_eq(1); 0-x_eq(2); 0-x_eq(3)],...
                                            [2-x_eq(1); 50-x_eq(2); 100-x_eq(3)]));
     tic
-    R = reachInnerConstrained(Sys, params,options,safeSet);
+    if sys.mode == "Ellipsoid_innerapproximation"
+        R = reachInnerConstrained(Sys, params,options,safeSet);
+    elseif sys.mode == "innerapproximation"
+        R = reachInner(Sys, params,options);
+    elseif sys.mode == "outerapproximation"
+        R = reach(Sys, params,options);
+    end
     tComp = toc;
     disp(['computation time of reachable set: ',num2str(tComp)]);
     
@@ -31,14 +37,5 @@ safeSet = zonotope(interval([0-x_eq(1); 0-x_eq(2); 0-x_eq(3)],...
     simOpt.points = 25;
     simOpt.type = 'gaussian';
     simRes = simulateRandom(Sys, params, simOpt);
-
-
-%     %Calculate final point
-%     i = 1;
-%     x_out = 0;
-%     for i = 1:25
-%         x_out = 0 + simRes.x{i,1}(end,:);
-%     end
-%     x_out = x_out/25;  
 
 end
